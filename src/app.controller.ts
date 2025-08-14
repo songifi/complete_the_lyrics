1d56eb2493b269ba661a0e012986e8073f271a93
mubarak23 <mubarakaminu340@gmail.com>
2025-08-14 13:41:50 +0100

    feat:handle health check on db connection

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,27 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import {
+  HealthCheck,
+  HealthCheckService,
+  TypeOrmHealthIndicator,
+} from '@nestjs/terminus'
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService,
+     private health: HealthCheckService,
+    private db: TypeOrmHealthIndicator,
+    ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+  @Get("/health")
+  @HealthCheck()
+  async check() {
+    return this.health.check([
+      async () => this.db.pingCheck('database'),
+    ]);
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TerminusModule } from '@nestjs/terminus';
 
 @Module({
   imports: [
@@ -9,6 +11,21 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true, 
       envFilePath: '.env', 
     }),
+      TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: true, // disable in production!
+      }),
+    }),
+    TerminusModule,
   ],
   controllers: [AppController],
   providers: [AppService],
